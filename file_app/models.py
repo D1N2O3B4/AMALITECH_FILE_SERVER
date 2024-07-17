27e42783e0c185d46a388e9ef5cf6bdbc38f4085
d1n2o3b4 <davebostons2017@gmail.com>
2024-07-17 08:16:20 +0000

    modified File model to check file format

--- file_app/models.py
+++ file_app/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
 # Create your models here.
+
+def check_file_format(value, file_format):
+    extension = value.name.split('.')[-1].lower()
+    valid_extensions = {
+        'image': ['jpg', 'jpeg', 'png', 'gif'],
+        'txt': ['txt'],
+        'pdf': ['pdf'],
+        'doc': ['doc', 'docx'],
+        'audio': ['mp3', 'wav'],
+        'video': ['mp4', 'avi', 'mov']
+    }
+    if extension not in valid_extensions.get(file_format, []):
+        raise ValidationError(f'Invalid file format for {file_format}.')
+
 class File(models.Model):
     FILE_FORMAT = (
         ('image', 'Image'),
@@ -23,7 +38,9 @@ class File(models.Model):
     email_count = models.PositiveIntegerField(default=0)
     uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='file')
 
-
+    def clean(self):
+        check_file_format(self.file, self.file_format)
+        super().clean()
 
     def __str__(self):
         return f"Filename: {self.title}| FileType:{self.file_format} | Download_count{self.download_count}| Email_count: {self.email_count}"
