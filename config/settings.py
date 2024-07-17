e1003e6bc704815b9ce6606db0088b3ed10ddf0d
d1n2o3b4 <davebostons2017@gmail.com>
2024-07-17 08:36:20 +0000

    added email credentials in settings.py

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ralphonline60@gmail.com'
+EMAIL_HOST_PASSWORD = 'gcmiqkouyhyihwhv'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'ralphonline60@gmail.com'
+SERVER_EMAIL = 'ralphonline60@gmail.com'
+
+PASSWORD_RESET_TIMEOUT = 14400
