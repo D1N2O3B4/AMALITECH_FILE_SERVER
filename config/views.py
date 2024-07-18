5069c0d43d340a4139acba979b06671ab828ff5f
d1n2o3b4 <davebostons2017@gmail.com>
2024-07-18 22:10:52 +0000

    added email_provider validation

--- config/views.py
+++ config/views.py
@@ -12,23 +12,33 @@ from .forms import CustomUserCreationForm
 
 
 def signup(request):
+    email_providers = ['gmail.com', 'outlook.com', 'yahoo.com', 'protonmail.com', 'icloud.com', 'aol.com', 'hotmail.com', 'msn.com', 'live.com', 'zoho.com', 'mail.com', 'gmx.com']
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            confirmation_token = default_token_generator.make_token(user)
-            confirmation_link = f"http://localhost:8000/confirm/{user.id}/{urlsafe_base64_encode(force_bytes(confirmation_token))}/"
+            user = form.save(commit=False)
+            email_provider = user.email.split('@')[-1]  
+            if email_provider in email_providers:
+                user.save()  
+                confirmation_token = default_token_generator.make_token(user)
+                confirmation_link = f"http://localhost:8000/confirm/{user.id}/{urlsafe_base64_encode(force_bytes(confirmation_token))}/"
 
-            send_mail(
-                subject="Confirm Your Email",
-                message=f"Click the link below to confirm your email:\n{confirmation_link}",
-                from_email=settings.SERVER_EMAIL,
-                recipient_list=[user.email],
-            )
-            messages.success(request, f"Account created for {user.email}. Please check your email for confirmation.")
+                send_mail(
+                    subject="Confirm Your Email",
+                    message=f"Click the link below to confirm your email:\n{confirmation_link}",
+                    from_email=settings.SERVER_EMAIL,
+                    recipient_list=[user.email],
+                )
+                messages.success(request, f"Account created for {user.email}. Please check your email for confirmation.")
+            else:
+                messages.error(request, "Invalid email provider.", extra_tags='Invalid email provider')
+                form = CustomUserCreationForm()
+        else:
+            messages.error(request, "Form is not valid.")
     else:
         form = CustomUserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
+
 
 class CustomLoginView(LoginView):
     authentication_form = EmailAuthenticationForm
